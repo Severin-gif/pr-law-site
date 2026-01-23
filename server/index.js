edde014492552d6b301e75a66ff74cc6382a8b96
Severin-gif <harvard000@gmail.com>
2026-01-23 15:50:32 +0300

    Update SPA fallback route

--- server/index.js
+++ server/index.js
@@ -176,7 +176,7 @@ app.post("/api/request-audit", leadHandler);
 
 // static + SPA fallback
 app.use(express.static(DIST_PATH, { index: false }));
-app.get(/^(?!\/api\/).*/, (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(DIST_PATH, "index.html"));
 });
 
