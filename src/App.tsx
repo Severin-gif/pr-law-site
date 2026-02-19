e93d2ba5b4d8eb1e9ca555a6773e1cd2d1608939
Severin-gif <harvard000@gmail.com>
2026-02-19 16:26:16 +0300

    16.26

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-import Header from "./layout/Header";
-import Footer from "./layout/Footer";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
