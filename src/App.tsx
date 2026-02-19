60e3af762a188b38d00b0058f25f099f8782df88
Severin-gif <harvard000@gmail.com>
2026-02-19 13:54:09 +0300

    13,54

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { Routes, Route } from "react-router-dom";
+
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
-import PartnersPage from "./components/partners";
 import ProBono from "./pages/ProBono";
+import PartnersPage from "./components/partners";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/partners" element={<PartnersPage />} />
-      <Route path="/pro-bono" element={<ProBono />} />
-    </Routes>
+    <>
+      <Header />
+
+      <main className="min-h-[calc(100vh-160px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/pro-bono" element={<ProBono />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </>
   );
 };
 
