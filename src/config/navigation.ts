a313d01924e7f94d6b8a96f26301006512d4095b
Severin-gif <harvard000@gmail.com>
2026-04-27 15:08:30 +0300

    Fix header nav to use absolute home anchors

---
+++ src/config/navigation.ts
@@ -0,0 +1,9 @@
+export const HEADER_NAV_LINKS = {
+  home: "/",
+  about: "/#about",
+  services: "/#services",
+  trust: "/#trust",
+  partners: "/partners",
+  proBono: "/pro-bono",
+  contact: "/#contact",
+} as const;
